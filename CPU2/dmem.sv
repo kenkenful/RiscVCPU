e3e946c1e564fd2ca10c3d3483cd98d91caa05b0
kenkenful <t0127mikaken@gmail.com>
2025-02-23 14:52:08 +0900

    Add files via upload

--- CPU2/dmem.sv
+++ CPU2/dmem.sv
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/22/2025 04:40:54 PM
-// Design Name: 
-// Module Name: mem
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module dmem(
     clk,
