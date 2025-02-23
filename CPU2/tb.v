993b33e04efd042f2a6debec44887136ee806cff
kenkenful <t0127mikaken@gmail.com>
2025-02-23 14:54:34 +0900

    Add files via upload

--- CPU2/tb.v
+++ CPU2/tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/09/2025 09:54:04 AM
-// Design Name: 
-// Module Name: tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb;
     localparam  SYSSTEP         = 8;
