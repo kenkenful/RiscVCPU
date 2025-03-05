3ebcbaf8cb434324df44fc79325c3a6e0c3c3da5
kenkenful <t0127mikaken@gmail.com>
2025-03-06 03:23:23 +0900

    Add files via upload

---
+++ CPU9/define.sv
@@ -0,0 +1,66 @@
+`ifndef _define_
+`define _define_
+
+typedef struct packed{
+    reg i_auipc ;
+    reg i_lui   ;
+    reg i_jal   ;
+    reg i_jalr  ;
+    reg i_beq   ;
+    reg i_bne   ;
+    reg i_blt   ;
+    reg i_bge   ;
+    reg i_bltu  ;
+    reg i_bgeu  ;
+    reg i_lb    ;
+    reg i_lh    ;
+    reg i_lw    ;
+    reg i_lbu   ;
+    reg i_lhu   ;
+    reg i_sb    ;
+    reg i_sh    ;
+    reg i_sw    ;
+    reg i_addi  ;
+    reg i_slti  ;
+    reg i_sltiu ;
+    reg i_xori  ;
+    reg i_ori   ;
+    reg i_andi  ;
+    reg i_slli  ;
+    reg i_srli  ;
+    reg i_srai  ;
+    reg i_add   ;
+    reg i_sub   ;
+    reg i_sll   ;
+    reg i_slt   ;
+    reg i_sltu  ;
+    reg i_xor   ;
+    reg i_srl   ;
+    reg i_sra   ;
+    reg i_or    ;
+    reg i_and   ;
+    reg i_fence ;
+    reg i_fencei;
+    reg i_ecall ;
+    reg i_ebreak;  
+    reg i_csrrw ;
+    reg i_csrrs ;
+    reg i_csrrc ;
+    reg i_csrrwi;
+    reg i_csrrsi;
+    reg i_csrrci;
+    reg i_mul   ;
+    reg i_mulh  ;
+    reg i_mulhsu;
+    reg i_mulhu ;
+    reg i_div   ;
+    reg i_divu  ;
+    reg i_rem   ;
+    reg i_remu  ;
+
+}de_ex_pipeline_reg;
+
+
+
+ 
+`endif
