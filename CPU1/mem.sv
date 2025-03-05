24c3aac6aa1b1be738dde154b7f8339de2fbc6fc
kenkenful <t0127mikaken@gmail.com>
2025-03-06 00:09:24 +0900

    Update mem.sv

--- CPU1/mem.sv
+++ CPU1/mem.sv
@@ -27,10 +27,7 @@ module mem(
     localparam addr_width = 12;
      
     reg [data_width-1:0] mem [2**addr_width-1:0];  // instruction and data melmory
-    
-    // Block RAM
-    //(* ram_style = "block" *)reg [data_width-1:0] mem [2**addr_width-1:0];  // instruction and data melmory
-    
+        
     initial begin 
         integer i = 0;
         $readmemh("/home/ttt/Desktop/riscv/RISCV/RISCV.srcs/sources_1/new/soft/test.hex", mem);
