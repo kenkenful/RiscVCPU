198a537fe575c9ce295a43898de1b7c465d922d6
kenkenful <t0127mikaken@gmail.com>
2025-03-06 01:28:24 +0900

    Update mem.sv

--- CPU1/mem.sv
+++ CPU1/mem.sv
@@ -86,7 +86,9 @@ module mem(
             5'b01111:begin
                 load_data = mem[mem_addr];
             end
-            default:;
+            default:begin
+                load_data = 0;    
+            end
         endcase
     end
     
