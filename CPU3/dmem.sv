d4ad691b7a8c65fbec28ed4abd84b72a5ab1129e
kenkenful <t0127mikaken@gmail.com>
2025-03-08 06:38:38 +0900

    Add files via upload

--- CPU3/dmem.sv
+++ CPU3/dmem.sv
@@ -2,8 +2,8 @@
 
 module dmem(
     clk,
-    wmem,
-    rmem,
+    is_store,
+    is_load,
     mem_addr,
     store_data,
     load_data,
@@ -11,8 +11,8 @@ module dmem(
     );
     
     input wire clk;
-    input wire [3:0] wmem;
-    input wire [4:0] rmem;
+    input wire is_store;
+    input wire is_load;
     input wire [31:0] mem_addr;
     input wire [31:0] store_data;
     output reg [31:0] load_data;
@@ -33,91 +33,14 @@ module dmem(
         end 
     end
     
-    reg [31:0] mem_out;
-    
     // load
     always_ff@(negedge clk) begin
-        mem_out = mem[mem_addr];
-    end
-    
-    always_comb begin
-        case (rmem) 
-        // unsgigned 1 byte
-        5'b00001:begin
-            load_data = {24'h0, mem_out[7:0]};
-        end
-        5'b00010:begin
-            load_data = {24'h0, mem_out[15:8]};
-        end
-        5'b00100:begin
-            load_data = {24'h0, mem_out[23:16]};
-        end
-        5'b01000:begin
-           load_data = {24'h0, mem_out[31:24]};
-        end 
-        // signed 1 byte
-        5'b10001:begin
-            load_data = {{24{mem_out[7]}}, mem_out[7:0]};
-        end
-        5'b10010:begin
-            load_data = {{24{mem_out[15]}}, mem_out[15:8]};
-        end
-        5'b10100:begin
-            load_data = {{24{mem_out[23]}}, mem_out[23:16]};
-        end
-        5'b11000:begin
-            load_data = {{24{mem_out[31]}}, mem_out[31:24]};
-        end 
-        // unsigned 2 bytes
-        5'b00011:begin
-            load_data = {16'b0, mem_out[15:0]};  
-        end
-        5'b01100:begin
-            load_data = {16'h0, mem_out[31:16]}; 
-        end
-        // signed 2 bytes
-        5'b10011:begin
-            load_data = {{16{mem_out[15]}}, mem_out[15:0]};  
-        end
-        5'b11100:begin
-            load_data = {{16{mem_out[31]}}, mem_out[31:16]}; 
-        end
-        // 4 bytes
-        5'b01111:begin
-            load_data = mem_out;
-        end
-        default:begin
-            load_data = 0;    
-        end
-        endcase
+        if(is_load) load_data = mem[mem_addr];
     end
     
     // store
     always_ff@(negedge clk)begin
-        case(wmem)
-          4'b0001:begin
-            mem[mem_addr] <= {24'b0, store_data[7:0]};
-          end
-          4'b0010:begin
-            mem[mem_addr] <= {16'b0, store_data[7:0], 8'b0};
-          end
-          4'b0100:begin
-            mem[mem_addr] <= {8'b0, store_data[7:0], 16'b0};
-          end
-          4'b1000:begin
-            mem[mem_addr] <= {store_data[7:0], 24'b0};
-          end
-          4'b0011:begin
-            mem[mem_addr] <= {16'b0, store_data[15:0]};
-          end
-          4'b1100:begin
-            mem[mem_addr] <= {store_data[15:0],16'b0};    
-          end
-          4'b1111:begin
-            mem[mem_addr] <= store_data[31:0];
-          end
-          default:;
-        endcase
+        if(is_store) mem[mem_addr] <= store_data;
     end
     
 endmodule
