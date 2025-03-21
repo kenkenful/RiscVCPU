86df1c03801ff3c1cd04460c909e7e6d2db00c0e
kenkenful <t0127mikaken@gmail.com>
2025-03-22 06:08:09 +0900

    Add files via upload

--- CPU5/riscv.sv
+++ CPU5/riscv.sv
@@ -1,8 +1,6 @@
 `default_nettype none
 `include "define.sv"
 
-`define UART_TX_ADDR 32'h20020
-
 module riscv(
       clk,
       reset,
@@ -157,22 +155,7 @@ module riscv(
     reg [31:0] pc_plus_ex;
 
     always_ff@(posedge clk)begin
-        if(is_jump)begin
-          a_ex         <= 0;
-          b_ex         <= 0;
-          jaloffset_ex <= 0;
-          broffset_ex  <= 0;
-          shamt_ex     <= 0;
-          simm_ex      <= 0;
-          uimm_ex      <= 0;
-          stimm_ex     <= 0;
-          rd_ex        <= 0;
-          rs1_ex       <= 0;
-          rs2_ex       <= 0;
-          pc_ex        <= 0;
-          pc_plus_ex   <= 0;
-          ex <= 0;
-        end else if(is_stoll)begin
+      if(is_stoll)begin
           a_ex         <= a_ex        ;
           b_ex         <= b_ex        ;
           jaloffset_ex <= jaloffset_ex;
@@ -187,22 +170,37 @@ module riscv(
           pc_ex        <= pc_ex;
           pc_plus_ex   <= pc_plus_ex;        
           ex           <= ex;
-        end else begin
-          a_ex         <= a_de        ;
-          b_ex         <= b_de        ;
-          jaloffset_ex <= jaloffset_de;
-          broffset_ex  <= broffset_de ;
-          shamt_ex     <= shamt_de    ;
-          simm_ex      <= simm_de     ;
-          uimm_ex      <= uimm_de     ;
-          stimm_ex     <= stimm_de    ;
-          rd_ex        <= rd_de       ;
-          rs1_ex       <= rs1_de      ;
-          rs2_ex       <= rs2_de      ;
-          pc_ex        <= pc;
-          pc_plus_ex   <= pc_plus;        
-          ex           <= de;
-        end
+      end else if(is_jump)begin
+          a_ex         <= 0;
+          b_ex         <= 0;
+          jaloffset_ex <= 0;
+          broffset_ex  <= 0;
+          shamt_ex     <= 0;
+          simm_ex      <= 0;
+          uimm_ex      <= 0;
+          stimm_ex     <= 0;
+          rd_ex        <= 0;
+          rs1_ex       <= 0;
+          rs2_ex       <= 0;
+          pc_ex        <= 0;
+          pc_plus_ex   <= 0;
+          ex <= 0;
+      end else begin
+        a_ex         <= a_de        ;
+        b_ex         <= b_de        ;
+        jaloffset_ex <= jaloffset_de;
+        broffset_ex  <= broffset_de ;
+        shamt_ex     <= shamt_de    ;
+        simm_ex      <= simm_de     ;
+        uimm_ex      <= uimm_de     ;
+        stimm_ex     <= stimm_de    ;
+        rd_ex        <= rd_de       ;
+        rs1_ex       <= rs1_de      ;
+        rs2_ex       <= rs2_de      ;
+        pc_ex        <= pc;
+        pc_plus_ex   <= pc_plus;        
+        ex           <= de;
+      end
     end
 
     // EXECUTE STATGE
@@ -380,7 +378,7 @@ module riscv(
                 default: store_data = 0;
               endcase
               
-              if(alu_out_ex == `UART_TX_ADDR) begin
+              if(alu_out_ex == UART_TX_ADDR) begin
                    uart_en = 1;
                    uart_tx_data = store_data[7:0];
               end
